a6bb5171ee82e11e284244c7f6fcec918afe4082
Tiago Carioca <tiagoacarioca@hotmail.com>
2020-12-13 11:59:32 -0400

    feat(backend): init & Route Appointment

---
+++ backend/src/routes/appointments.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
+
+const appointmentsRouter = Router();
+
+const appointments: { id: string; provider: string; date: number }[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
