c00de77bfe36f2863489649b208dc18d99137e24
Tiago Carioca <tiagoacarioca@hotmail.com>
2021-01-20 18:10:22 -0400

    Feat(backend): Error Handling

--- backend/src/services/CreateAppointementService.ts
+++ backend/src/services/CreateAppointementService.ts
@@ -1,11 +1,13 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
@@ -20,7 +22,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked!');
+      throw new AppError('This appointment is already booked!');
     }
 
     const appointment = appointmentsRepository.create({
